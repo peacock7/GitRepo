49a947fbd9620dd2798115eaff209d705d0c21d9
peacock7 <s.kaaviyashri@gmail.com>
2024-03-31 00:44:47 +0530

    add a new code to Shri branch

--- src/org/repo/GitDemo.java
+++ src/org/repo/GitDemo.java
@@ -2,7 +2,15 @@ package org.repo;
 
 public class GitDemo {
 	
+	private void branch2() {
+		System.out.println("Branch2 code");
+
+	}
+	
 	public static void main(String[] args) {
+		
+		GitDemo d =new GitDemo();
+		d.branch2();
 		
 		System.out.println("Demo code");
 	}
